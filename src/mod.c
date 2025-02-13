5a215e710c0c85d35ec4e4070bddd057bc5213bb
ThatHypedPerson <thathypedperson@gmail.com>
2025-02-12 21:20:15 -0800

    Fix Blue Warp Softlock + Bump Version

--- src/mod.c
+++ src/mod.c
@@ -6,7 +6,7 @@ extern u8 gPlayerFormItemRestrictions[PLAYER_FORM_MAX][114];
 // Patches every form's restriction to allow for the use of normal masks
 RECOMP_CALLBACK("*", recomp_after_play_init) void modify_item_restriction_table(PlayState* this) {
     for (int form = PLAYER_FORM_FIERCE_DEITY; form <= PLAYER_FORM_DEKU; form++) {
-        for (int mask = ITEM_MASK_TRUTH; mask <= ITEM_MASK_GIANT; mask++) {
+        for (int mask = ITEM_MASK_DEKU; mask <= ITEM_MASK_GIANT; mask++) {
             gPlayerFormItemRestrictions[form][mask] = true;
         }
     }
@@ -16,8 +16,12 @@ RECOMP_CALLBACK("*", recomp_after_play_init) void modify_item_restriction_table(
 RECOMP_HOOK("Player_Init") void after_player_init(Actor* thisx, PlayState* play) {
     Player* this = (Player*)thisx;
     
-    // if statement fixes owl statue saved masks
+    // fix setting owl statue saved masks
     if (gSaveContext.save.equippedMask == PLAYER_MASK_NONE) {
-        gSaveContext.save.equippedMask = this->currentMask;
+        // fix blue warp softlock
+        if (this->currentMask != PLAYER_MASK_DEKU && this->currentMask != PLAYER_MASK_GORON &&
+            this->currentMask != PLAYER_MASK_ZORA && this->currentMask != PLAYER_MASK_FIERCE_DEITY) {
+            gSaveContext.save.equippedMask = this->currentMask;
+        }
     }
 }
---
+++ thunderstore/CHANGELOD.md
@@ -0,0 +1,6 @@
+## 1.1.0
+* Allow Fierce Diety to switch into other forms.
+* Fix softlock that occurs after entering blue warps.
+
+## 1.0.0
+* Initital release
